4cc6c4a5c9c2ef099c9c86b27bc497c548e33bda
nikolay <killaris97@mail.ru>
2022-09-04 02:52:10 +0300

    learning examples of enhancers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,51 @@
-import { createStore } from 'redux';
+import { legacy_createStore as createStore, compose } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+// Enhancers are not using in real practice! Below are examples just for learning...
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            })
+        }
+        return originalDispatch(action);
+    };
+
+    return store;
+};
+
+const logEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        console.log(action.type);
+        return originalDispatch(action);
+    };
+
+    return store;
+};
+
+// MONKEY-PATCHING. BAD PRACTICE
+/*
+const originalDispatch = store.dispatch;
+store.dispatch = (action) => {
+    if (typeof action === 'string') {
+        return originalDispatch({
+            type: action
+        })
+    }
+    return originalDispatch(action);
+};
+*/
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+
+store.dispatch("HELLO WORLD!");
 
 export default store;
